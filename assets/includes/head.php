4299af775cf428d119472e9d76910fa9366dc810
Your Name <you@example.com>
2022-10-11 16:16:33 +0200

    test

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name = "author" lang = "fr" content = "Le ric" >
-  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+
   <meta name = "catégorie" content = "Application de vote" >
   <!-- Font Awesome -->
 <link
