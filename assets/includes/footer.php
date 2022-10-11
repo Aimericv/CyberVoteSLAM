8ce3bbe886eb2c191c7aacb3ca11b121050736b8
Your Name <you@example.com>
2022-10-11 19:37:24 +0200

    footer

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -6,14 +6,22 @@
         <p>Site de gestion des votes français !</p>
 
       </div>
-      <!-- Grid column -->
-
       <hr class="clearfix w-100 d-md-none pb-3">
-
-      <!-- Grid column -->
       <div class="col-md-3 mb-md-0 mb-3">
+        <h5 class="text-uppercase">Lien utiles:</h5>
 
-        <!-- Links -->
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!"><i class="fas fa-hammer"></i> Mentions légales </a>
+          </li>
+          <li>
+            <a href="https://www.elections.interieur.gouv.fr/comprendre-elections/pourquoi-je-vote/droit-de-vote-et-son-importance"><i class="fas fa-balance-scale-left"></i> Droits de vote</a>
+          </li>
+        </ul>
+
+      </div>
+
+      <div class="col-md-3 mb-md-0 mb-3">
         <h5 class="text-uppercase">Links</h5>
 
         <ul class="list-unstyled">
@@ -30,50 +38,14 @@
             <a href="#!">Link 4</a>
           </li>
         </ul>
-
       </div>
-      <!-- Grid column -->
-
-      <!-- Grid column -->
-      <div class="col-md-3 mb-md-0 mb-3">
-
-        <!-- Links -->
-        <h5 class="text-uppercase">Links</h5>
-
-        <ul class="list-unstyled">
-          <li>
-            <a href="#!">Link 1</a>
-          </li>
-          <li>
-            <a href="#!">Link 2</a>
-          </li>
-          <li>
-            <a href="#!">Link 3</a>
-          </li>
-          <li>
-            <a href="#!">Link 4</a>
-          </li>
-        </ul>
-
-      </div>
-      <!-- Grid column -->
-
     </div>
-    <!-- Grid row -->
 
   </div>
-  <!-- Footer Links -->
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Copyright:
-    <a href="/"> MDBootstrap.com</a>
+  <div class="footer-copyright text-center py-3">© 2022 Copyright:
   </div>
-  <!-- Copyright -->
-
-<!-- Footer -->
     <script nomodule src="./assets/js/bootstrap.min.js"></script>
     <script nomodule src="./assets/js/jquery.js"></script>
-    <!-- MDB -->
 <script
   type="text/javascript"
   src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.js"
