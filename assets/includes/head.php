70c4a13e53675f562a131a829577ccddd5a8badb
Your Name <you@example.com>
2022-10-11 18:43:15 +0200

    modification

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -9,11 +9,7 @@
   <link rel="stylesheet" href="./assets/css/icon.css">
   <link rel="stylesheet" href="./assets/css/googlefonnt.css">
   <link rel="stylesheet" href="./assets/css/mdb.css">
-
-
   <meta name = "catégorie" content = "Application de vote" >
-
-
-  <title>CyberVote | Site internet qui permet de voter de manière anonyme</title>
+  <title>CyberVote | Site internet qui permet de voter de manière automatique</title>
 </head>
 <body>
--- assets/includes/header.php
+++ assets/includes/header.php
@@ -7,13 +7,13 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Accueil </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Voter</a>
+        <a class="nav-link" href="vote.php">Voter</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Résultat</a>
+        <a class="nav-link" href="resultat.php">Résultat</a>
       </li>
      
       </li>
