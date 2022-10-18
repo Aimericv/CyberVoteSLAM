0dfd65901b7668d89e73b3dd65b7f3179957e039
root <root@SLAM2-IP-196.fenelonsio.local>
2022-10-18 15:50:06 +0200

    commit message

---
+++ assets/includes/config.php
@@ -0,0 +1,5 @@
+<?php
+define('DB_HOST', 'localhost'); // Hôte
+define('DB_NAME', 'cybervote'); // Nom de la base de donnée
+define('DB_USER', 'cybervote'); // Nom d'utilisateur
+define('DB_PASS', 'cybervote'); // Mot de passe
