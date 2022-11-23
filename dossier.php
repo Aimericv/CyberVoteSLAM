62d88220214f9287ed9dd11f061d23c0b2cbcc06
root <root@DESKTOP-805819E.localdomain>
2022-11-23 17:43:06 +0100

    super maj mdr

--- dossier.php
+++ dossier.php
@@ -1,6 +1,7 @@
 <?php
 include("./assets/includes/head.php");
 include("./assets/includes/header.php");
+include("./assets/includes/config.php");
 if (!isset($_SESSION['login'])) {
 	header ('Location: login.php');
 	exit();
@@ -11,22 +12,51 @@ if (!isset($_SESSION['login'])) {
     <div class="main-block">
       <h1>Inscription de dossier</h1>
       <form action="/" method="post">
-        
-        <label id="icon" for="prenom"><i class="fas fa-user"></i></label>
-        <input type="text" name="prenom" id="prenom" placeholder="Prenom" required/>
-        <label id="icon" for="nom"><i class="fas fa-user"></i></label>
-        <input type="text" name="nom" id="nom" placeholder="Nom" required/>
-        <label id="icon" for="code_postale"><i class="fas fa-location-dot"></i></label>
-        <input type="text" name="code_postale" id="nom" placeholder="Code Postal" required/>
-        <label id="icon" for="date_naissance"><i class="fas fa-birthday-cake"></i></label>
-        <input type="text" name="date_naissance" id="nom" placeholder="Date de naissance" required/>
-        <div class="btn-block">
-        <button type="submit" method="post" onclick="javascript: form.action='';">Envoyer</button>
+
+  <div class="row mb-4">
+    <div class="col">
+      <div class="form-outline">
+        <input type="text" id="form3Example1" class="form-control"  name="prenom"/>
+        <label class="form-label" for="form3Example1">Prénom</label>
+      </div>
+    </div>
+    <div class="col">
+      <div class="form-outline">
+        <input type="text" id="form3Example2" class="form-control" name="nom"/>
+        <label class="form-label" for="form3Example2">Nom</label>
+      </div>
+    </div>
+  </div>
+
+  <div class="form-outline mb-4">
+    <input type="text" id="form3Example3" class="form-control" name="code_postale" require/>
+    <label class="form-label" for="form3Example3">Code postale</label>
+  </div>
+
+  <div class="form-outline mb-4">
+    <input type="date" id="form3Example4" class="form-control" name="date_naissance" />
+    <label class="form-label" for="form3Example4">Date de naissance</label>
+  </div>
+
+
+
+  <button type="submit" class="btn btn-primary btn-block mb-4" method="post" onclick="javascript: form.action='';">Valider les informations</button>
+
+ 
         
         <?php
-        
-          
+          $servername = DB_SERVER;
+          $username = DB_USERNAME;
+          $password = DB_PASSWORD;
+          $dbname = DB_NAME;
+          $a= $_POST['nom'];
+          $b= $_POST['prenom'];
+          $c= $_POST['code_postale'];
+          $d= $_POST['date_naissance'];
+
           try {
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // définir le mode exception d'erreur PDO 
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             
             $sql = "INSERT INTO `Electeur` ( `nom`, `prenom`, `code_postale`, `date_naissance`)
@@ -43,7 +73,7 @@ if (!isset($_SESSION['login'])) {
           $conn = null;
           ?>
          
-        
+         </form>
         </div>
       </form>
     </div>
