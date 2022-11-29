00c9efc81bac7569117217e0ad3409da1860a280
Your Name <you@example.com>
2022-11-29 19:59:11 +0100

    the big maj ta capté chakal

--- resultat.php
+++ resultat.php
@@ -1,6 +1,7 @@
 <?php
 include("./assets/includes/head.php");
 include("./assets/includes/header.php");
+include("./assets/includes/config.php");
 ?>
   <div class="card">
   <div class="card-body">
@@ -8,46 +9,38 @@ include("./assets/includes/header.php");
     <p class="card-text">
     <div class="container">
   <div class="row">
-    <div class="col-md">
-      One of three columns
-    </div>
-    <div class="col-md">
-      One of three columns
-    </div>
-    <div class="col-md">
-      One of three columns
-    </div>
+ 
   </div>
 </div>
   <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Nom:</th>
+      <th scope="col">Prénom:</th>
+      <th scope="col">Nombre de votes:</th>
     </tr>
   </thead>
   <tbody>
-    <tr class="table-active">
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-  </tbody>
+									<?php
+										$services = $conn->query("SELECT * from Resultat ORDER BY nb_vote DESC LIMIT 10");
+										if ($services->num_rows > 0) {
+										    while($row = $services->fetch_assoc()) {
+									?>
+									<tr>
+										<td><?= $row['nom'] ?></td>
+										<td><?= $row['prenom'] ?></td>
+										<td><?= $row['nb_vote'] ?></td>
+									</tr>
+									<?php }
+										} else {
+									?>
+									<tr>
+										<td>-</td>
+										<td>-</td>
+										<td>-</td>
+									</tr>
+									<?php }	?>
+								</tbody>
 </table>
 </p>
 <button type="button" class="btn btn-primary">Voir plus <i class="fa-solid fa-plus"></i></button>
