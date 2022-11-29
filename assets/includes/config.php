a6a5573e6dcb3ee6ae8d73ab565799d458a521b1
Your Name <you@example.com>
2022-11-29 20:01:31 +0100

    the big maj ta capté chakal

--- assets/includes/config.php
+++ assets/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'cybervote');
 define('DB_PASSWORD', 'cybervote');
 define('DB_NAME', 'cybervote');
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
