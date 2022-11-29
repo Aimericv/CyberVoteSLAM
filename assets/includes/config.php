b3a452280e2ec014fe5260738d0cac48808b0b77
Q7i <48409701+Q7i@users.noreply.github.com>
2022-11-29 15:21:05 +0100

    Update config.php

--- assets/includes/config.php
+++ assets/includes/config.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'a1c4cca6');
+define('DB_USERNAME', 'cybervote');
+define('DB_PASSWORD', 'cybervote');
 define('DB_NAME', 'cybervote');
 $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($conn === false){
