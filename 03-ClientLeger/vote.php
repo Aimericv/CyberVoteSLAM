f94f2ace9a1c0437867c3cd19263f494ebeef589
Aimericv <aimericv17@gmail.com>
2022-12-13 15:17:11 +0100

    Nouvelle page vote

--- 03-ClientLeger/vote.php
+++ 03-ClientLeger/vote.php
@@ -2,8 +2,8 @@
 include("./assets/includes/head.php");
 include("./assets/includes/header.php");
 if (!isset($_SESSION['login'])) {
-	header ('Location: login.php');
-	exit();
+    header ('Location: login.php');
+    exit();
 }
 
 
@@ -28,29 +28,25 @@ $resultat = $mysqli->query($requete);
 
 
 while ($ligne = $resultat->fetch_assoc()) {
- //var_dump($ligne);
+
   echo $ligne['nom'] . ' ' . $ligne['prenom'] . '<form action="" method="post" name="">
-  <input type="hidden" value="' .$ligne['Id_candidat'].'">
-  <input type="submit" value="voter">
+
+  <input type="submit" name ="' .$ligne['Id_candidat'].'" value="voter">
   </form>'.'<br>';
  
 }
 
+$sql ="UPDATE Resultat SET nb_vote=nb_vote+1 WHERE Id_candidat='1'" ;
+$resultat2 = $mysqli->query($sql);
 
- $sql = "INSERT INTO `Resultat` ( `nb_vote`, `id_election`, `id_candidat`, `nom`, `prenom`)
- VALUES( '1', '', '', '', '')
- ";
-$resultat = $mysqli->query($sql);
+
+
+ 
 
 
 
 $mysqli->close();
-//"INSERT INTO `Vote` (`Id_candidat`,`Id_Electeur`,`id_election`,`vote`) values (1, 666, 1, 1)
 
-// # compte
-// select count(*) AS "RESULTAT", id_candidat from Vote where id_election=1 GROUP by Id_candidat;
 
-// # Inserer un nouveau vote
-// insert into Vote (Id_candidat,Id_Electeur,id_election, vote) values (1, 666, 1, '');
 
 ?>
